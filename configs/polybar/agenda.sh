77c90017d600f4f09d49396b0690e4f0ae40e8d9
Zander <me@zanderf.net>
2023-02-08 14:02:35 -0600

    fix: 12h time for i3-agenda

--- configs/polybar/agenda.sh
+++ configs/polybar/agenda.sh
@@ -5,10 +5,11 @@ AGENDA=$(python3 ~/.config/i3/i3-agenda/i3_agenda/i3_agenda.py \
     --show-event-before 120 \
     --hide-event-after 10 \
     -ttl 60 \
-    --no-event-text ""
+    --no-event-text "" \
     2>/dev/null)
 
 if [ -n "$AGENDA" ] && [[ $AGENDA != "Traceback"* ]]; then
+    AGENDA=$(sed 's/\([12][0-9]\):\([0-9][0-9]\)/echo "$((\1 % 12 == 0 ? 12 : \1 % 12)):\2"/ge' <<< "$AGENDA")
     echo "󰃭  $AGENDA"
 else
     echo ""
