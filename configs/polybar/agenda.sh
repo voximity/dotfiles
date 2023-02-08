65922f67e788588ae6f7b23cad454b0bad569018
Zander <me@zanderf.net>
2023-02-08 15:10:35 -0600

    fix: agenda 12h sed

--- configs/polybar/agenda.sh
+++ configs/polybar/agenda.sh
@@ -9,7 +9,7 @@ AGENDA=$(python3 ~/.config/i3/i3-agenda/i3_agenda/i3_agenda.py \
     2>/dev/null)
 
 if [ -n "$AGENDA" ] && [[ $AGENDA != "Traceback"* ]]; then
-    AGENDA=$(sed 's/\([12][0-9]\):\([0-9][0-9]\)/echo "$((\1 % 12 == 0 ? 12 : \1 % 12)):\2"/ge' <<< "$AGENDA")
+    AGENDA=$(sed -E 's/([12][0-9]):([0-9][0-9])/"$((\1 % 12 == 0 ? 12 : \1 % 12)):\2"/ge' <<<"echo $AGENDA")
     echo "󰃭  $AGENDA"
 else
     echo ""
