b0a157afd932933a400346c6baabd7d7a7f97256
Zander <me@zanderf.net>
2023-02-10 14:53:33 -0600

    i3-agenda hide event after 0

--- configs/polybar/agenda.sh
+++ configs/polybar/agenda.sh
@@ -3,7 +3,7 @@
 AGENDA=$(python3 ~/.config/i3/i3-agenda/i3_agenda/i3_agenda.py \
     -c ~/.config/i3/i3-agenda-credentials.json \
     --show-event-before 120 \
-    --hide-event-after 10 \
+    --hide-event-after 0 \
     -ttl 60 \
     --no-event-text "" \
     2>/dev/null)
